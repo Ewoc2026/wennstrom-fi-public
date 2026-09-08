80b191909da5b0cee281fea9b0e7a3ea1e297fd8
Jyri Wennström <jyriwee@pm.me>
2026-09-09 01:19:55 +0300

    Use wennstrom.fi as production site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://ewoc2026.github.io',
-  base: '/wennstrom-fi-public',
+  site: 'https://wennstrom.fi',
 });
